d7b5a924cfd38ac734cc70f3ebbf1aca1a39450a
KillYourTV <killyourtv@mail.i2p>
2012-04-05 09:53:03 +0000

    Make setup.py more Windows friendly, tidy up manifest

--- setup.py
+++ setup.py
@@ -4,18 +4,34 @@ if sys.hexversion < int(0x020600f0):
 	sys.exit(1)
 
 from distutils.core import setup
-setup(name='grid-updates',
-        version='1.0.0a',
-        py_modules=['gridupdates'],
-        description='Tahoe-LAFS helper script',
-        author=['darrob','KillYourTV'],
-        author_email=['darrob@mail.i2p', 'killyourtv@mail.i2p'],
-        url='http://darrob.i2p/grid-updates',
-        license='Public Domain',
-        data_files=[('share/grid-updates', ['etc/NEWS.atom.template',  'etc/news-stub.html',
-            'etc/pandoc-template.html',  'etc/tahoe.css.original',  'etc/tahoe.css.patched',
+import platform
+import shutil
+
+if platform.system() == 'Windows':
+    try:
+        import py2exe
+    except ImportError:
+        pass
+
+    shutil.copy('scripts/grid-updates', 'scripts/grid-updates.py')
+    script_name = 'scripts/grid-updates.py'
+else:
+    script_name = 'scripts/grid-updates'
+
+setup(name = 'grid-updates',
+        version = '1.0.0a',
+        py_modules = ['gridupdates'],
+        description = 'Tahoe-LAFS helper script',
+        author = ['darrob','KillYourTV'],
+        author_email = ['darrob@mail.i2p', 'killyourtv@mail.i2p'],
+        url = 'http://darrob.i2p/grid-updates',
+        license = 'Public Domain',
+        data_files = [('share/grid-updates', ['etc/NEWS.atom.template',
+            'etc/news-stub.html', 'etc/pandoc-template.html',
+            'etc/tahoe.css.original',  'etc/tahoe.css.patched',
             'etc/welcome.xhtml.original', 'etc/welcome.xhtml.patched']),
             ('share/man/man1', ['man/grid-updates.1'])],
-        scripts=['scripts/grid-updates'],
+        scripts = [script_name],
+        console = [script_name],
         )
 
