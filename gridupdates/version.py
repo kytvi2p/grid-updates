5b460f812eca116a25d417a03bd9d1072dad7811
KillYourTV <killyourtv@mail.i2p>
2012-04-29 00:47:05 +0000

    bump patch version due to script-uri change

--- gridupdates/version.py
+++ gridupdates/version.py
@@ -4,4 +4,4 @@ __maintainer__ = ['darrob', 'KillYourTV']
 __email__ = ['darrob@mail.i2p', 'killyourtv@mail.i2p']
 __status__ = "Development"
 __version__ = "1.0.0git"
-__patch_version__ = '1.8.3-gu5'
+__patch_version__ = '1.8.3-gu6'
