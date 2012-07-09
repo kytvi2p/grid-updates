d06d9dbb535fef694d86174d5a819b2c645a1163
darrob <darrob@mail.i2p>
2012-07-09 10:24:55 +0000

    Update patch version number in version.py
    Also switch to simple digits for version numbers.

--- gridupdates/version.py
+++ gridupdates/version.py
@@ -4,4 +4,4 @@ __maintainer__ = ['darrob', 'KillYourTV']
 __email__ = ['darrob@mail.i2p', 'killyourtv@mail.i2p']
 __status__ = "Development"
 __version__ = "1.1.3"
-__patch_version__ = '1.8.3-gu6'
+__patch_version__ = 'gu7'
